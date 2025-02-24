806577533254e9794673861ecbbdce9c47e92d0c
dylanabke <dja5837@psu.edu>
2025-02-24 18:28:51 -0500

    Finishing the counter-app requirements

--- counter-app2.js
+++ counter-app2.js
@@ -8,18 +8,18 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
 /**
  * `counter-app2`
- * 
+ *
  * @demo index.html
  * @element counter-app2
  */
 export class CounterApp2 extends DDDSuper(I18NMixin(LitElement)) {
-
   static get tag() {
     return "counter-app2";
   }
 
   constructor() {
     super();
+    this.count = 0;
     this.title = "";
     this.t = this.t || {};
     this.t = {
@@ -29,55 +29,99 @@ export class CounterApp2 extends DDDSuper(I18NMixin(LitElement)) {
     this.registerLocalization({
       context: this,
       localesPath:
-        new URL("./locales/counter-app2.ar.json", import.meta.url).href +
-        "/../",
+        new URL("./locales/counter-app2.ar.json", import.meta.url).href + "/../",
       locales: ["ar", "es", "hi", "zh"],
     });
   }
 
-  // Lit reactive properties
   static get properties() {
     return {
       ...super.properties,
-      title: { type: String },
+      count: { type: Number, reflect: true },
     };
   }
 
-  // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--counter-app2-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
+    return [
+      super.styles,
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        :host([count="18"]) {
+          color: var(--ddd-theme-default-athertonViolet);
+        }
+        :host([count="21"]) {
+          color: var(--ddd-theme-default-athertonViolet);
+        }
+        :host([count="22"]) {
+          color: var(--ddd-theme-default-error);
+        }
+        :host([count="-22"]) {
+          color: var(--ddd-theme-default-error);
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+        }
+        .counter {
+          font-size: var(--counter-app2-label-font-size, var(--ddd-font-size-xxl));
+        }
+      `,
+    ];
   }
 
-  // Lit render the HTML
+  updated(changedProperties) {
+    super.updated(changedProperties);
+    if (changedProperties.has("count")) {
+      if (this.count === 21) {
+        this.makeItRain();
+      }
+    }
+  }
+
+  makeItRain() {
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        setTimeout(() => {
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
+
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+      <confetti-container id="confetti" class="wrapper">
+        <div class="counter">${this.count}</div>
+        <div class="buttons">
+          <button @click="${this.decrease}">-1</button>
+          <button @click="${this.increase}">+1</button>
+        </div>
+      </confetti-container>
+    `;
   }
 
-  /**
-   * haxProperties integration via file reference
-   */
+  increase() {
+    if (this.count < 22){
+      this.count++;
+    }
+  }
+  decrease() {
+    if (this.count > -22){
+      this.count--;
+    }
+
+  }
+  reset() {
+    this.count = 0;
+  }
+
   static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url).href;
   }
 }
 
